4f9647a81d3639922201badd85b8e2237fadbca0
truemanburbank <juxtap.se@gmail.com>
2024-11-02 21:28:38 +0900

    5639

---
+++ 5639.py
@@ -0,0 +1,54 @@
+import sys
+sys.setrecursionlimit(20000)
+input = sys.stdin.readline
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            current = self.root
+            while True:
+                if current.data > data:
+                    if current.left is None:
+                        current.left = Node(data)
+                        break
+                    current = current.left
+                if current.data < data:
+                    if current.right is None:
+                        current.right = Node(data)
+                        break
+                    current = current.right
+
+    def postOrder(self, node=None):
+        global answer
+        if node is None:
+            node = self.root
+
+        if node.left is not None:
+            self.postOrder(node.left)
+        if node.right is not None:
+            self.postOrder(node.right)
+        answer.append(node.data)
+
+
+tree = Tree()
+while True:
+    try:
+        tree.add(int(input()))
+    except:
+        break
+answer = []
+tree.postOrder()
+print('\n'.join(map(str, answer)))
